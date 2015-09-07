a333d6cd701a6c648a3d5a3768b33064a8c41ee2
Georgi Markov <markovood85@gmail.com>
2015-09-07 21:49:31 +0300

    Added solution for Numeral Systems lecture
    Added new solution and the first problem

---
+++ 4.NumeralSystems/Problem1.DecimalToBinary/DecimalToBinary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+internal class DecimalToBinary
+{
+    // Write a program to convert decimal numbers to their binary representation.
+    private static void Main()
+    {
+        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+        Console.Write("Please enter a number in decimal representation: ");
+        int userInput = int.Parse(Console.ReadLine());
+
+        ConvertDecimalToBinary(userInput);
+    }
+
+    private static void ConvertDecimalToBinary(int userInput)
+    {
+        int remain = 0;
+        string tempBinaryRepr = "";
+        string binaryRepr = "";
+
+        for (int i = userInput; i > 0; i /= 2)
+        {
+            remain = i % 2;
+            tempBinaryRepr += remain;
+        }
+
+        // reversing
+        for (int i = tempBinaryRepr.Length - 1; i >= 0; i--)
+        {
+            binaryRepr += tempBinaryRepr[i];
+        }
+
+        Console.WriteLine("{0} in decimal system is\n{1} in binary system", userInput, binaryRepr);
+    }
+}
---
+++ 4.NumeralSystems/Problem2.BinaryToDecimal/BinaryToDecimal.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Problem2.BinaryToDecimal
+{
+    class BinaryToDecimal
+    {
+        // Write a program to convert binary numbers to their decimal representation.
+        static void Main()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+            Console.Write("Please enter a number in binary representation: ");
+            string userInput = Console.ReadLine();
+
+            ConvertBinaryToDecimal(userInput);
+        }
+
+        private static void ConvertBinaryToDecimal(string userInput)
+        {
+            Validate(userInput);
+            double decimalNum = 0;
+            int lastIndex = userInput.Length - 1;
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                decimalNum += userInput[lastIndex] * (Math.Pow(2, i));
+                lastIndex--;
+            }
+
+            Console.WriteLine("{0} in binary system is\n{1} in decimal system", userInput, decimalNum);
+        }
+
+        private static void Validate(string userInput)
+        {
+            bool validInput = false;
+            while (!validInput)
+            {
+                foreach (char symbol in userInput)
+                {
+                    if (symbol != '0' && symbol != '1')
+                    {
+                        Console.Write("Wrong format!!!\nBinary representations can only have \"0\"s or \"1\"s\nTry again: ");
+                        userInput = Console.ReadLine();
+                        break;
+                    }
+                    else
+                    {
+                        validInput = true;
+                    }
+                }
+            }
+        }
+    }
+}
