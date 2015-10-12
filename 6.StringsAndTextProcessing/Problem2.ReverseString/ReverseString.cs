58070a4c5a4913140755555724c98486e87e93c5
Georgi Markov <markovood85@gmail.com>
2015-10-12 22:25:10 +0300

    started new lecture
    begin of strings and text processing lecture

---
+++ 6.StringsAndTextProcessing/Problem2.ReverseString/ReverseString.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+class ReverseString
+{
+    // Write a program that reads a string, reverses it and prints the result at the console.
+    static void Main()
+    {
+        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+
+    }
+}
