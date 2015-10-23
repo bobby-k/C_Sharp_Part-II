25690dcef3168afa7ef7a534fadbc48a2e984f35
Georgi Markov <markovood85@gmail.com>
2015-10-23 20:19:02 +0300

    added completed problem16 and begining of problem17

--- 6.StringsAndTextProcessing/Problem16.DateDifference/DateDifference.cs
+++ 6.StringsAndTextProcessing/Problem16.DateDifference/DateDifference.cs
@@ -1,49 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
-class DateDifference
+internal class DateDifference
 {
     // Write a program that reads two dates in the format: day.month.year and calculates the number of days between them.
-    static void Main()
+    private static void Main()
     {
-        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+        Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("bg-BG");
 
-        DateTime fromDate = GetDate();// make these global
-        DateTime toDate = GetDate();// make these global
+        DateTime fromDate = GetDate(); 
+        DateTime toDate = GetDate(); // if GetDate(); returns DateTime.MinValue that means one or both dates didn't parse successful
 
         while (fromDate == DateTime.MinValue || toDate == DateTime.MinValue)
         {
             if (fromDate == DateTime.MinValue)
             {
-                Console.WriteLine("The first date you entered is wrong!!!\nMake sure it matches the format mm/dd/yyyy");
+                Console.WriteLine("The first date you entered is wrong!!!\nMake sure it matches the format dd.mm.yyyy");
                 fromDate = GetDate();
             }
             else
             {
-                Console.WriteLine("The second date is wrong!!!\nMake sure it matches the format mm/dd/yyyy");
+                Console.WriteLine("The second date is wrong!!!\nMake sure it matches the format dd.mm.yyyy");
                 toDate = GetDate();
             }
         }
 
-        ValidateToDate(fromDate, toDate);
+        toDate = ValidateToDate(fromDate, toDate);
 
         Console.Clear();
         Console.WriteLine("From: " + fromDate.ToString("dd/MM/yyyy"));
         Console.WriteLine("To: " + toDate.ToString("dd/MM/yyyy"));
         TimeSpan days = toDate - fromDate;
         Console.WriteLine("There are {0} days past between the two dates.", days.Days);
-
-
     }
 
-    static DateTime GetDate()
+    private static DateTime GetDate()
     {
-        Console.Write("Enter a date in the following format 'mm/dd/yyyy': ");
+        Console.Write("Enter a date in the following format 'dd.mm.yyyy': ");
         string inputDate = Console.ReadLine();
 
         DateTime date;
@@ -55,12 +49,14 @@ class DateDifference
         return date;
     }
 
-    static void ValidateToDate(DateTime dateFrom,DateTime dateTo)
+    private static DateTime ValidateToDate(DateTime dateFrom, DateTime dateTo)
     {
         while (dateTo < dateFrom)
         {
             Console.WriteLine("NOTICE:The second date must be a period after the first date!\nPlease enter a date after {0}", dateFrom.ToString("dd/MM/yyyy"));
             dateTo = GetDate();
         }
+
+        return dateTo;
     }
 }
---
+++ 6.StringsAndTextProcessing/Problem17.DateInBulgarian/DateInBulgarian.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+class DateInBulgarian
+{
+    // Write a program that reads a date and time given in the format: day.month.year hour:minute:second and prints the date and time
+    // after 6 hours and 30 minutes (in the same format) along with the day of week in Bulgarian.
+    static void Main()
+    {
+        Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("bg-BG");
+    }
+}
