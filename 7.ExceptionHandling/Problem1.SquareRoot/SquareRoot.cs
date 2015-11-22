950c89838b720dbf12447ba5dd8e3acb52e86f6f
Georgi Markov <markovood85@gmail.com>
2015-11-22 14:32:47 +0200

    added next lecture
    added first two problems of the lecture

---
+++ 7.ExceptionHandling/Problem1.SquareRoot/SquareRoot.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+internal class SquareRoot
+{
+    // Write a program that reads an integer number and calculates and prints its square root. If the number is invalid or negative,
+    // print Invalid number. In all cases finally print Good bye. Use try-catch-finally block.
+    private static void Main()
+    {
+        Console.Write("Please enter an integer number here: ");
+
+        try
+        {
+            int num = int.Parse(Console.ReadLine());
+            if (num < 0)
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                double result = Math.Sqrt(num);
+                Console.WriteLine(result);
+            }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid number...");
+        }
+        catch (ArgumentException)
+        {
+            Console.WriteLine("Invalid number...");
+        }
+        finally
+        {
+            Console.WriteLine("Good Bye.");
+        }
+    }
+}
---
+++ 7.ExceptionHandling/Problem2.EnterNumbers/EnterNumbers.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+internal class EnterNumbers
+{
+    // Write a method ReadNumber(int start, int end) that enters an integer number in a given range [start...end]. If an invalid number
+    // or non-number text is entered, the method should throw an exception. Using this method write a program that enters 10 numbers:
+    // a1, a2, ... a10, such that 1 < a1 < ... < a10 < 100
+
+    private const int Start = 1;
+    private const int End = 100;
+    private static int[] tenNumbers = new int[10];
+
+    private static void Main()
+    {
+        try
+        {
+            Console.WriteLine("Please enter ten numbers between 1 and 100");
+            EnterTenNumbers();
+            Print(tenNumbers);
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine("Start again!");
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine("Start again!");
+        }
+    }
+
+    private static int[] EnterTenNumbers()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write("number {0} = ", i + 1);
+            tenNumbers[i] = ReadNumber(Start, End);
+        }
+
+        return tenNumbers;
+    }
+
+    private static int ReadNumber(int start, int end)
+    {
+        int userNumber = int.Parse(Console.ReadLine());
+        if (userNumber < start || userNumber > end)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        return userNumber;
+    }
+
+    private static void Print(int[] array)
+    {
+        Console.WriteLine(string.Join(", ", tenNumbers));
+    }
+}
