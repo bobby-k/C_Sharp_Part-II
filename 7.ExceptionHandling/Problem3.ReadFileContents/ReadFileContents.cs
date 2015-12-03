f2f4343042d31be796e1e4fb422e282511c1165b
Georgi Markov <markovood85@gmail.com>
2015-12-03 20:42:49 +0200

    final solutions
    added last two problems solved

--- 7.ExceptionHandling/Problem3.ReadFileContents/ReadFileContents.cs
+++ 7.ExceptionHandling/Problem3.ReadFileContents/ReadFileContents.cs
@@ -1,16 +1,82 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
+using System.Security;
 using System.Text;
-using System.Threading.Tasks;
 
-class ReadFileContents
+internal class ReadFileContents
 {
     // Write a program that enters file name along with its full file path (e.g. C:\WINDOWS\win.ini), reads its contents and prints it
     // on the console. Find in MSDN how to use System.IO.File.ReadAllText(…). Be sure to catch all possible exceptions and print
     // user-friendly error messages.
-    static void Main()
+    private static void Main()
     {
+        Console.WriteLine("Enter the full path to the file including it");
+        string path = Console.ReadLine();
 
+        Console.Clear();
+        ReadFileContent(path);
+    }
+
+    private static void ReadFileContent(string path)
+    {
+        try
+        {
+            string content = File.ReadAllText(path, Encoding.Default);
+            Console.WriteLine(content);
+        }
+        catch (ArgumentNullException ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine("Try again!");
+            Main();
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine("Try again!");
+            Main();
+        }
+        catch (PathTooLongException ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine("Try again!");
+            Main();
+        }
+        catch (DirectoryNotFoundException ex)
+        {
+            Console.WriteLine(ex.Data);
+            Console.WriteLine("Try again!");
+            Main();
+        }
+        catch (FileNotFoundException ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine("Try again!");
+            Main();
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine("Try again!");
+            Main();
+        }
+        catch (UnauthorizedAccessException ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine("Try again!");
+            Main();
+        }
+        catch (NotSupportedException ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine("Try again!");
+            Main();
+        }
+        catch (SecurityException ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine("Try again!");
+            Main();
+        }
     }
 }
---
+++ 7.ExceptionHandling/Problem4.DownloadFile/DownloadFile.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+
+internal class DownloadFile
+{
+    // Write a program that downloads a file from Internet (e.g. Ninja image) and stores it the current directory. Find in Google how
+    // to download files in C#. Be sure to catch all exceptions and to free any used resources in the finally block.
+    private static void Main()
+    {
+        Console.WriteLine("Enter the file (name and extension) to download");
+        string file = Console.ReadLine();   // news-img01.png
+
+        Console.WriteLine("Enter the address to that file");
+        string URL = Console.ReadLine();    // http://telerikacademy.com/Content/Images/news-img01.png
+
+        WebClient wc = new WebClient();
+        try
+        {
+            wc.DownloadFile(URL, file);
+
+            Console.Clear();
+            Console.WriteLine("DONE");
+            Console.WriteLine("(file is saved in the application's start directory)");
+        }
+        catch (ArgumentNullException ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine("Try again!");
+            Main();
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine("Try again!");
+            Main();
+        }
+        catch (WebException ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine("Try again!");
+            Main();
+        }
+        catch (NotSupportedException ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine("Try again!");
+            Main();
+        }
+        finally
+        {
+            wc.Dispose();
+        }
+    }
+}
