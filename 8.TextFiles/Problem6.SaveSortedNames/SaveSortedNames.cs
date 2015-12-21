223695108da057c668460e6ecca3d3d8acb802ef
Georgi Markov <markovood85@gmail.com>
2015-12-21 21:44:19 +0200

    added more problems solved
    still working on problem 7

---
+++ 8.TextFiles/Problem6.SaveSortedNames/SaveSortedNames.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+internal class SaveSortedNames
+{
+    // Write a program that reads a text file containing a list of strings, sorts them and saves them to another text file.
+    private static void Main()
+    {
+        string pathIn = @"..\..\Files\input.txt";
+        string pathOut = @"..\..\Files\output.txt";
+        var fileContent = GetFileContent(pathIn);
+        var sortedContent = SortFileContent(fileContent);
+        PrintToFile(pathOut, sortedContent);
+    }
+
+    private static List<string> GetFileContent(string path)
+    {
+        var fileContent = new List<string>();
+        var reader = new StreamReader(path);
+
+        using (reader)
+        {
+            string line = reader.ReadLine();
+            while (line != null)
+            {
+                fileContent.Add(line);
+
+                line = reader.ReadLine();
+            }
+        }
+
+        return fileContent;
+    }
+
+    private static List<string> SortFileContent(List<string> content)
+    {
+        content.Sort();
+
+        return content;
+    }
+
+    private static void PrintToFile(string path, List<string> content)
+    {
+        var writer = new StreamWriter(path);
+
+        using (writer)
+        {
+            for (int i = 0; i < content.Count; i++)
+            {
+                writer.WriteLine(content[i]);
+            }
+        }
+    }
+}
---
+++ 8.TextFiles/Problem7.ReplaceSub-string/ReplaceSubString.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+class ReplaceSubString
+{
+    // Write a program that replaces all occurrences of the sub-string start with the sub-string finish in a text file. Ensure it will
+    // work with large files (e.g. 100 MB).
+    static void Main()
+    {
+        string path = @"..\..\Files\input.txt";
+
+        string old = "start";
+        string _new = "finish";
+
+        var reader = new StreamReader(path);
+
+        using (reader)
+        {
+            string line = reader.ReadLine();
+
+            while (line != null)
+            {
+                line.Replace()
+
+                line = reader.ReadLine();
+            }
+        }
+    }
+
+
+}
